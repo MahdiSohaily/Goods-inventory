e4caa129dc26df511abe5be12a195275728319dc
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-09 13:56:55 +0330

    Add search result table structure

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,15 +1,13 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Components/Welcome.vue";
 </script>
 
 <template>
     <AppLayout title="Search">
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
+            <div class="max-w-full mx-auto sm:px-6 lg:px-8">
+                <Welcome />
             </div>
         </div>
     </AppLayout>

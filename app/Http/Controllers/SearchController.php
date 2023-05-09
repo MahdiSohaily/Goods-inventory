ac7e1df07cf66d097aa5c334ad814029410d668b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-09 15:50:58 +0330

    Add sql command to get data from database base on serial number

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,17 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
 
     public function search(Request $request)
     {
-        if ($mode) {
-            $sql="SELECT * FROM nisha WHERE partnumber LIKE '".$key."%'";
-        } else {
-            $sql="SELECT * FROM nisha WHERE partnumber LIKE '".$key."%'";
-        }
-        return response()->json([$request->all()]);
+        $pattern = $request->input('pattern');
+        $user = DB::table('nisha')->where('partnumber', 'like', "$pattern%") ->get();
+        return response()->json([$user]);
     }
 }
--- app/Models/Good.php
+++ app/Models/Good.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Good extends Model
 {
     use HasFactory;
+    protected $table = 'nisha';
 }

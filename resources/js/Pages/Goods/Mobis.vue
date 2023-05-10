a91678d65639786b0206a5655d50903643832f0f
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-10 12:59:58 +0330

    Add mobis page

---
+++ resources/js/Pages/Goods/Mobis.vue
@@ -0,0 +1,240 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Components/Welcome.vue";
+defineProps({ rates: Object });
+</script>
+
+<template>
+    <AppLayout title="Search">
+        <div class="py-12">
+            <div class="max-w-full mx-auto sm:px-6 lg:px-8">
+                <div>
+                    <div class="p-6 lg:p-8 flex justify-center">
+                        <input type="text" name="serial" id="serial"
+                            class="rounded-md py-3 text-center w-96 border-2 bg-gray-100" min="0" max="30"
+                            @keyup="search($event.target.value, rates)" placeholder="... کد فنی قطعه را وارد کنید" />
+                    </div>
+                    <div class="flex justify-center items-center pb-6">
+                        <input type="checkbox" name="super" id="mode" class="rounded-md" />
+                        <label for="mode" class="ml-1">Super Mode</label>
+                    </div>
+                    <div class="bg-gray-100 bg-opacity-25">
+                        <div class="max-w-7xl overflow-x-auto mx-auto">
+                            <table class="min-w-full text-left text-sm font-light">
+                                <thead class="font-medium dark:border-neutral-500">
+                                    <tr class="bg-green-700">
+                                        <th scope="col" class="px-3 py-3 bg-black text-white w-72 text-center">
+                                            شماره فنی
+                                        </th>
+                                        <th scope="col" class="px-3 py-3 text-white w-32">
+                                            دلار پایه
+                                        </th>
+                                        <th scope="col" class="px-3 py-3 text-white border-black border-r-2">
+                                            +10%
+                                        </th>
+
+                                        <!-- START TO Loop over the existing rates at database -->
+                                        <th v-for="item in rates" :class="item.status" scope="col"
+                                            class="px-3 py-3 text-white text-center">
+                                            {{ item.amount }}
+                                        </th>
+                                        <!-- END the loop -->
+
+                                        <th scope="col" class="px-3 py-3 text-white w-52 text-center">
+                                            عملیات
+                                        </th>
+                                        <th scope="col" class="px-3 py-3 text-white">
+                                            وزن
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody id="results">
+                                    {{
+                                        result ? result : ""
+                                    }}
+                                    <tr class="transition duration-300 ease-in-out hover:bg-neutral-200">
+                                        <td class="whitespace-nowrap px-3 py-3 font-bold">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                    </tr>
+                                    <tr class="transition duration-300 ease-in-out hover:bg-neutral-200">
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                    </tr>
+                                    <tr class="transition duration-300 ease-in-out hover:bg-neutral-200">
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-3 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-3">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-3">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-3">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-3 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-3">
+                                            Mark
+                                        </td>
+                                    </tr>
+                                    <tr class="transition duration-300 ease-in-out hover:bg-neutral-200">
+                                        <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            Mark
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            Otto
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            @mdo
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4 font-medium">
+                                            1
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            Mark
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                >
+            </div>
+        </div>
+    </AppLayout>
+</template>

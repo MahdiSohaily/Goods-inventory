d3297e52bc63120443931fd1550a64fc50363e62
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-10 15:26:38 +0330

    Add command to update nisha mobis if exist

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -28,13 +28,26 @@ class SearchController extends Controller
     public function mobis(string $mobis = null)
     {
         $good = DB::table('nisha')->where('partnumber', '=', "$mobis")->first();
+        $result = $this->checkMobis($mobis, $good);
 
+        return $result;
+    }
+
+    public function get_http_response_code($url)
+    {
+        ini_set('user_agent', 'Mozilla/5.0');
+        $headers = get_headers($url);
+        return substr($headers[0], 9, 3);
+    }
+
+    public function checkMobis($mobis, $good)
+    {
         $context = stream_context_create(array("http" => array("header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36")));
 
         if ($this->get_http_response_code("https://partsmotors.com/products/$mobis") != "200") {
 
             DB::update(
-                "update nisha set mobis = '-' where partnumber = '?'",
+                "update nisha set mobis = '-' where partnumber = ?",
                 [$mobis]
             );
             return false;
@@ -44,7 +57,7 @@ class SearchController extends Controller
             $html = file_get_contents("https://partsmotors.com/products/$mobis", false, $context);
 
             $html = str_get_html($html);
-
+            $price = null;
             foreach ($html->find('meta[property=og:price:amount]') as $element) {
                 $price = $element->content;
             }
@@ -57,12 +70,6 @@ class SearchController extends Controller
                 [$price, $mobis]
             );
         }
-    }
-
-    function get_http_response_code($url)
-    {
-        ini_set('user_agent', 'Mozilla/5.0');
-        $headers = get_headers($url);
-        return substr($headers[0], 9, 3);
+        return $good;
     }
 }

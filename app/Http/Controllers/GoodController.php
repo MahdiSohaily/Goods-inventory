5b5230fd8b4592fc1ad08300627cb0333616700c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-11 09:28:00 +0330

    Add index function to render the goods index page

--- app/Http/Controllers/GoodController.php
+++ app/Http/Controllers/GoodController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Inertia\Inertia;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GoodController extends Controller
 {
-    //
+    public function index () {
+        $goods = DB::table('rates')
+            ->select('amount', 'status')
+            ->orderBy('amount', 'asc')
+            ->get();
+        return Inertia::render('Dashboard', ['goods' => $goods]);
+    }
 }

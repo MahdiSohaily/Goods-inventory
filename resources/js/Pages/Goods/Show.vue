eadb4e7024d9bac41448daf68c160de425eb044b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-11 10:59:14 +0330

    Add print function to display data

--- resources/js/Pages/Goods/Show.vue
+++ resources/js/Pages/Goods/Show.vue
@@ -31,18 +31,133 @@ const page = async (action) => {
     }
 };
 
-const getData = (page) => {
+const getData = (page, pattern) => {
+    const resultBox = document.getElementById("results");
     axios
         .post("/search", {
             pattern,
-            superMode,
+            page,
         })
         .then(function (response) {
-            resultBox.innerHTML = print(response.data, rates);
+            resultBox.innerHTML = print(response.data);
         })
         .catch(function (error) {
             console.log(error);
         });
+};
+
+const print = (data, rates) => {
+    let template = "";
+    if (data[0].length > 0) {
+        for (let item of data[0]) {
+            const partNumber = item.partnumber;
+            let price = item.price;
+            let avgPrice = Math.round((price * 110) / 243.5);
+            const weight = Number(item.weight).toFixed(1);
+            const mobis = item.mobis;
+            let status;
+
+            if (mobis == "0.00") {
+                status = "NO-Price";
+            } else if (mobis == "-") {
+                status = "NO-Mobis";
+            } else if (mobis == null) {
+                status = "Request";
+            } else {
+                status = "YES-Mobis";
+            }
+
+            template += `<tr class="transition duration-300 ease-in-out hover:bg-neutral-200">
+                <td class='whitespace-nowrap bg-blue-900'>
+                <div class='flex gap-1 text-white font-bold'>`;
+            if (status == "Request") {
+                template +=
+                    ` <a class='link-s ml-4 Request' target='_blank' href='mobis/` +
+                    partNumber +
+                    `'>?</a>`;
+            } else if (status == "NO-Price") {
+                template +=
+                    ` <a class='link-s ml-4 NO-Price' target='_blank' href='mobis/` +
+                    partNumber +
+                    `'>!</a>`;
+            } else if (status == `NO-Mobis`) {
+                template +=
+                    ` <a class='link-s ml-4 NO-Mobis' target='_blank' href='mobis/` +
+                    partNumber +
+                    `'>x</a>`;
+            } else {
+                template += `<span class='ml-11'></span>`;
+            }
+
+            template +=
+                partNumber +
+                `</div></td>
+                <td class='whitespace-nowrap text-center px-3 py-3'>` +
+                Math.round(avgPrice * 1.1) +
+                `</td>
+                <td class='orange whitespace-nowrap text-center px-3 py-3 border-black border-r-2' >` +
+                Math.round(avgPrice * 1.2) +
+                `</td>`;
+
+            template += normalRate(avgPrice, rates);
+
+            template +=
+                `
+                <td class='whitespace-nowrap w-24'>
+                    <div class='flex justify-center gap-1 items-center px-2'>
+                    <a target='_blank' href='https://www.google.com/search?tbm=isch&q=` +
+                partNumber +
+                `'>
+                        <img class='w-5 h-auto' src='/img/google.png' alt='google'>
+                    </a>
+                    <a msg='` +
+                partNumber +
+                `'>
+                        <img class='w-5 h-auto' src='/img/tel.png' alt='part'>
+                    </a>
+                    <a target='_blank' href='https://partsouq.com/en/search/all?q=` +
+                partNumber +
+                `'>
+                        <img class='w-5 h-auto' src='/img/part.png' alt='part'>
+                    </a>
+                </div>
+                </td>
+                <td class='whitespace-nowrap px-3 py-3 kg'>
+                    <div class='weight'>` +
+                weight +
+                ` KG</div>
+                </td>
+            </tr> `;
+
+            if (status == "YES-Mobis") {
+                price = mobis;
+                price = price.replace(",", "");
+                avgPrice = Math.round((price * 110) / 243.5);
+                template +=
+                    `<tr class='bg-gray-300 transition duration-400 ease-in-out hover:bg-neutral-500'>
+                        <td class='text-white font-bold pl-12'> ` +
+                    partNumber +
+                    `-M</td>
+                        <td class='font-bold whitespace-nowrap text-center px-3 py-3'>` +
+                    Math.round(avgPrice) +
+                    `</td>
+                        <td class='whitespace-nowrap px-3 py-3 text-center border-black border-r-2'>` +
+                    Math.round(avgPrice * 1.1) +
+                    `</td>`;
+                template += mobisRate(avgPrice, rates);
+                template += `<td></td><td></td></tr>`;
+            }
+        }
+        return template;
+    } else {
+        return `<tr v-else class="transition duration-300 ease-in-out bg-neutral-200">
+                    <td colspan="14" class="whitespace-nowrap px-3 py-3 text-center font-bold text-orange-500">
+                        <i class="material-icons text-red-500">mood_bad</i>
+                        <br>
+                                !قطعه ای برای  کد فنی وارد در سیستم پیدا نشد
+                    </td>
+                </tr>`;
+    }
 };
 </script>
 

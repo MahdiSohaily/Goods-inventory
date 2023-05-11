dfcbb0e29d2700a9ccc0a7aa1ea7c838fd6d2790
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-11 09:19:50 +0330

    Add routes for goods manipulation

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SearchController;
+use App\Http\Controllers\GoodController;
 use Inertia\Inertia;
 
 /*
@@ -36,6 +37,17 @@ Route::middleware([
         Route::get('/dashboard', 'index')->name('dashboard'); // Dashboard route lead us to the search page
         Route::post('/search', 'search')->name('search'); // Route to manage searched path
         Route::get('/mobis/{mobis}', 'mobis')->name('mobis'); // Route to check if a good has mobis
+    });
+
+
+    // Good Page related routs
+    Route::controller(GoodController::class)->group(function () {
+        Route::get('/goods', 'index')->name('goods'); // goods route lead us to the goods page
+        Route::get('/goods/create', 'create')->name('goods.create'); // Route to show the form for creating a new good in the system
+        Route::post('/goods', 'store')->name('goods.store'); // Route to create new good in the system
+        Route::put('/goods/{goods}/edit', 'edit')->name('goods.edit'); // Route to show the selected good for update
+        Route::put('/goods/{goods}', 'update')->name('goods.update'); // Route to update an existing good
+        Route::delete('/goods/{goods}', 'delete')->name('goods.delete'); // Route to delete a specific good
     });
 
     Route::get('/goods', function () {

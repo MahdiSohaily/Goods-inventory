3e7a78d84d1e5755d496f8418a571b8b2d08605b
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-13 11:27:47 +0330

    Add the index controller to render view

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class RateController extends Controller
 {
-    //
+    public function index()
+    {
+        $rate = DB::table('rates')->get();
+        Inertia::render('Rates/Show', ['rates' => $rate]);
+    }
 }

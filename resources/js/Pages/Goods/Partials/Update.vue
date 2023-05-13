a98a5121546ced3322cdcd663571be69613f7d2a
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-13 10:32:32 +0330

    Add the update page html structure

---
+++ resources/js/Pages/Goods/Partials/Update.vue
@@ -0,0 +1,133 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import { useForm } from "@inertiajs/vue3";
+import ActionMessage from "@/Components/ActionMessage.vue";
+import FormSection from "@/Components/FormSection.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+
+const form = useForm({
+    _method: "POST",
+    serial: null,
+    price: null,
+    weight: null,
+    mobis: null,
+});
+
+const createGood = () => {
+    form.post(route("goods.store"), {
+        errorBag: "createGood",
+        preserveScroll: true,
+        onSuccess: () => clearInput(),
+    });
+};
+const clearInput = () => {
+    serial.value.value = null;
+    price.value.value = null;
+    weight.value.value = null;
+    mobis.value.value = null;
+};
+</script>
+
+<template>
+    <AppLayout title="Goods">
+        <div>
+            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+                <FormSection @submitted="createGood">
+                    <template #title> Create New Good </template>
+
+                    <template #description>
+                        Fill the following form fields carefully in order to
+                        register new good in the system.
+                    </template>
+
+                    <template #form>
+                        <!-- Serial Number -->
+                        <div class="col-span-6 sm:col-span-4">
+                            <InputLabel for="serial" value="Part Number" />
+                            <TextInput
+                                required
+                                id="serial"
+                                v-model="form.serial"
+                                type="text"
+                                class="mt-1 block w-full"
+                                autocomplete="serial"
+                            />
+                            <InputError
+                                :message="form.errors.serial"
+                                class="mt-2"
+                            />
+                        </div>
+
+                        <!-- Price -->
+                        <div class="col-span-6 sm:col-span-4">
+                            <InputLabel for="price" value="Price" />
+                            <TextInput
+                                required
+                                id="price"
+                                v-model="form.price"
+                                type="text"
+                                class="mt-1 block w-full"
+                                autocomplete="username"
+                            />
+                            <InputError
+                                :message="form.errors.price"
+                                class="mt-2"
+                            />
+                        </div>
+                        <!-- Weight -->
+                        <div class="col-span-6 sm:col-span-4">
+                            <InputLabel for="weight" value="Weight" />
+                            <TextInput
+                                required
+                                id="weight"
+                                v-model="form.weight"
+                                type="text"
+                                class="mt-1 block w-full"
+                                autocomplete="username"
+                            />
+                            <InputError
+                                :message="form.errors.weight"
+                                class="mt-2"
+                            />
+                        </div>
+                        <!-- Mobis -->
+                        <div class="col-span-6 sm:col-span-4">
+                            <InputLabel for="mobis" value="Mobis" />
+                            <TextInput
+                                required
+                                id="mobis"
+                                v-model="form.mobis"
+                                type="text"
+                                class="mt-1 block w-full"
+                                autocomplete="username"
+                            />
+                            <InputError
+                                :message="form.errors.mobis"
+                                class="mt-2"
+                            />
+                        </div>
+                    </template>
+
+                    <template #actions>
+                        <ActionMessage
+                            :on="form.recentlySuccessful"
+                            class="mr-3"
+                        >
+                            Saved.
+                        </ActionMessage>
+
+                        <PrimaryButton
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                        >
+                            Save
+                        </PrimaryButton>
+                    </template>
+                </FormSection>
+            </div>
+        </div>
+    </AppLayout>
+</template>

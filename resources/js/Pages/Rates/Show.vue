154cded4249bd7f78ac59d93512f2005983a4eb8
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-13 14:12:27 +0330

    Add edit feature

--- resources/js/Pages/Rates/Show.vue
+++ resources/js/Pages/Rates/Show.vue
@@ -12,64 +12,40 @@ defineProps({
             <table class="min-w-full text-left text-sm font-light">
                 <thead class="font-medium dark:border-neutral-500">
                     <tr class="bg-green-700">
-                        <th
-                            scope="col"
-                            class="px-3 py-3 text-white text-center"
-                        >
+                        <th scope="col" class="px-3 py-3 text-white text-center">
                             Rate
                         </th>
                         <th scope="col" class="px-3 py-3 text-white text-center">
                             Status
                         </th>
-                        <th
-                            scope="col"
-                            class="px-3 py-3 text-white text-center"
-                        >
+                        <th scope="col" class="px-3 py-3 text-white text-center">
                             Color
                         </th>
-                        <th
-                            scope="col"
-                            class="px-3 py-3 text-white text-center"
-                        >
+                        <th scope="col" class="px-3 py-3 text-white text-center">
                             عملیات
                         </th>
                     </tr>
                 </thead>
                 <tbody id="results">
-                    <tr
-                        v-if="rates.length > 0"
-                        v-for="item in rates"
-                        class="transition duration-300 ease-in-out bg-neutral-200 hover:bg-neutral-100"
-                    >
-                        <td
-                            class="whitespace-nowrap px-3 py-3 text-center font-bold"
-                        >
+                    <tr v-if="rates.length > 0" v-for="item in rates"
+                        class="transition duration-300 ease-in-out bg-neutral-200 hover:bg-neutral-100">
+                        <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
                             {{ item.amount }}
                         </td>
-                        <td
-                            class="whitespace-nowrap px-3 py-3 text-center font-bold"
-                        >
+                        <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
                             {{ item.status }}
                         </td>
-                        <td
-                            class="whitespace-nowrap px-3 py-3 text-center font-bold" :class="item.status"
-                        >
-                            {{  }}
+                        <td class="whitespace-nowrap px-3 py-3 text-center font-bold" :class="item.status">
+                            {{ }}
                         </td>
                         <td class="whitespace-nowrap w-24">
-                            <div
-                                class="flex justify-center gap-1 items-center px-2"
-                            >
+                            <div class="flex justify-center gap-1 items-center px-2">
                                 <a :href="route('rates.edit', item.id)">
-                                    <i class="material-icons text-blue-500"
-                                        >create</i
-                                    >
+                                    <i class="material-icons text-blue-500">create</i>
                                 </a>
                                 <form @submit.prevent="remove(item.id)">
                                     <button type="submit">
-                                        <i class="material-icons text-red-600"
-                                            >delete_forever</i
-                                        >
+                                        <i class="material-icons text-red-600">delete_forever</i>
                                     </button>
                                 </form>
                             </div>

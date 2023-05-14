68182798d04ec8bb90933f004b06f87745a95fe1
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-14 15:07:52 +0330

    Add comments to the code to be more describtive

--- resources/js/Pages/Relation/Show.vue
+++ resources/js/Pages/Relation/Show.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Link, router, useForm } from '@inertiajs/vue3';
 import AppLayout from "@/Layouts/AppLayout.vue";
 import SectionBorder from '@/Components/SectionBorder.vue';
 
@@ -62,7 +63,7 @@ const prepareData = (data) => {
         if (item.pattern) {
             template +=
                 `<div class='w-full flex justify-between items-center shadow-md hover:shadow-lg rounded-md px-4 py-3 mb-2 border-1 border-gray-300' id='search-` + item.id + `'>
-                    <p class='text-sm'>` + item.partNumber + `</p>
+                    <p class='text-sm font-semibold text-gray-600'>` + item.partNumber + `</p>
                     <i onclick='load(event,`+ item.pattern + `)'  
                         data-id='` + item.id + `' 
                         data-partNumber='` + item.partNumber + `'
@@ -72,7 +73,7 @@ const prepareData = (data) => {
         } else {
             template +=
                 `<div class='w-full flex justify-between items-center shadow-md hover:shadow-lg rounded-md px-4 py-3 mb-2 border-1 border-gray-300' id='search-` + item.id + `'>
-                    <p class='text-sm'>` + item.partNumber + `</p>
+                    <p class='text-sm font-semibold text-gray-600'>` + item.partNumber + `</p>
                     <i onclick='add(event)'  
                         data-id='` + item.id + `' 
                         data-partNumber='` + item.partNumber + `'
@@ -165,27 +166,27 @@ function load(event, pattern_id) {
 <template>
     <AppLayout title="Profile">
         <div class="h-70S grid grid-cols-1 my-8 md:grid-cols-3 gap-6 lg:gap-8 p-6 lg:p-8">
-            <div class="bg-white rounded-md shadow-md p-3">
-                <div class="flex items-center justify-between">
+            <div class="bg-white rounded-lg shadow-md">
+                <div class="flex items-center justify-between p-3">
                     <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2">
-                        <i class="material-icons text-orange-400">search</i>
+                        <i class="material-icons font-semibold text-orange-400">search</i>
                         Search Goods
                     </h2>
                 </div>
 
-                <div class="flex justify-center py-3">
+                <div class="flex justify-center px-3">
                     <input type="text" name="serial" id="serial"
-                        class="rounded-md py-3 w-full border-1 text-sm border-gray-500 focus:outline-none text-gray-500"
+                        class="rounded-md py-3 w-full border-1 text-sm border-gray-300 focus:outline-none text-gray-500"
                         min="0" max="30" @keyup="search($event.target.value, rates)" placeholder="Part Number ..." />
                 </div>
                 <SectionBorder />
-                <div id="search_result">
+                <div id="search_result" class="p-3">
                     <!-- Search Results are going to be appended here -->
                 </div>
             </div>
 
-            <div class="bg-white rounded-md shadow-md p-3">
-                <div class="flex items-center justify-between">
+            <div class="bg-white rounded-lg shadow-md">
+                <div class="flex items-center justify-between p-3">
                     <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2">
                         <i class="material-icons text-green-600">beenhere</i>
                         Selected Items
@@ -194,31 +195,21 @@ function load(event, pattern_id) {
                         Clear All
                     </button>
                 </div>
-
-                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
-                    Laracasts offers thousands of video tutorials on Laravel,
-                    PHP, and JavaScript development. Check them out, see for
-                    yourself, and massively level up your development skills in
-                    the process.
+                <p class="px-3 mb-4 text-gray-500 text-sm leading-relaxed">
+                    List of the selected items to be added on relation!.
                 </p>
+                <SectionBorder />
 
-                <p class="mt-4 text-sm">
-                    <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
-                        Start watching Laracasts
-
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
-                            <path fill-rule="evenodd"
-                                d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
-                                clip-rule="evenodd" />
-                        </svg>
-                    </a>
-                </p>
+                <div class="p-3">
+                    <!-- selected items are going to be added here -->
+                    here
+                </div>
             </div>
 
-            <div class="bg-white rounded-md shadow-md p-3">
+            <div class="bg-white rounded-lg shadow-md">
                 <div class="">
                     <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2">
-                        <i class="material-icons text-blue-500">save</i>
+                        <i class="material-icons font-semibold text-blue-500">save</i>
                         Register Relation
                     </h2>
                 </div>

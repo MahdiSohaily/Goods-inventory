a4c4a2933e3103f2f62f4ee50ba457e341b75e59
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-14 11:25:27 +0330

    Add the relation page sections

--- resources/js/Pages/Relation/Show.vue
+++ resources/js/Pages/Relation/Show.vue
@@ -9,51 +9,71 @@ defineProps({
 
 <template>
     <AppLayout title="Profile">
-        <div class="max-w-5xl mx-auto py-20 sm:px-6 lg:px-8">
-            <table class="min-w-full text-left text-sm font-light">
-                <thead class="font-medium dark:border-neutral-500">
-                    <tr class="bg-green-700">
-                        <th scope="col" class="px-3 py-3 text-white text-center">
-                            Rate
-                        </th>
-                        <th scope="col" class="px-3 py-3 text-white text-center">
-                            Status
-                        </th>
-                        <th scope="col" class="px-3 py-3 text-white text-center">
-                            Color
-                        </th>
-                        <th scope="col" class="px-3 py-3 text-white text-center">
-                            عملیات
-                        </th>
-                    </tr>
-                </thead>
-                <tbody id="results">
-                    <tr v-if="rates.length > 0" v-for="item in rates"
-                        class="transition duration-300 ease-in-out bg-neutral-200 hover:bg-neutral-100">
-                        <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
-                            {{ item.amount }}
-                        </td>
-                        <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
-                            {{ item.status }}
-                        </td>
-                        <td class="whitespace-nowrap px-3 py-3 text-center font-bold" :class="item.status">
-                            {{}}
-                        </td>
-                        <td class="whitespace-nowrap w-24">
-                            <div class="flex justify-center gap-1 items-center px-2">
-                                <a :href="route('rates.edit', item.id)">
-                                    <i class="material-icons text-blue-500">create</i>
-                                </a>
-                                <form @submit.prevent="remove(item.id)">
-                                    <button type="submit">
-                                        <i class="material-icons text-red-600">delete_forever</i>
-                                    </button>
-                                </form>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+        <div class="h-70S bg-gray-200 bg-opacity-25 grid grid-cols-1 my-8 md:grid-cols-3 gap-6 lg:gap-8 p-6 lg:p-8">
+            <div class="bg-white rounded-md shadow-md">
+                <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+                    </svg>
+                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                        <a href="https://laravel.com/docs">Documentation</a>
+                    </h2>
+                </div>
+
+                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                    Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
+                </p>
+
+                <p class="mt-4 text-sm">
+                    <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700">
+                        Explore the documentation
+
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
+                            <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </p>
+            </div>
+
+            <div class="bg-white rounded-md shadow-md">
+                <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
+                        <path stroke-linecap="round" d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z" />
+                    </svg>
+                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                        <a href="https://laracasts.com">Laracasts</a>
+                    </h2>
+                </div>
+
+                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                    Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
+                </p>
+
+                <p class="mt-4 text-sm">
+                    <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
+                        Start watching Laracasts
+
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
+                            <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </p>
+            </div>
+
+            <div class="bg-white rounded-md shadow-md">
+                <div class="flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>
+                    <h2 class="ml-3 text-xl font-semibold text-gray-900">
+                        <a href="https://tailwindcss.com/">Tailwind</a>
+                    </h2>
+                </div>
+
+                <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+                    Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful framework at your fingertips.
+                </p>
+            </div>
         </div>
     </AppLayout>
 </template>

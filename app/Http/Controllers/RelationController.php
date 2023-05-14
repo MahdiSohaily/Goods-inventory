b616cee93ba169b9715b909e713039b9daadd439
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-14 11:09:22 +0330

    Add the relation controller function index function to render UI

--- app/Http/Controllers/RelationController.php
+++ app/Http/Controllers/RelationController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class RelationController extends Controller
 {
-    //
+    public function index()
+    {
+        $status = DB::table('status')->get();
+        $cars = DB::table('cars')->get();
+        return Inertia::render('Relation/Show', ['status' => $status, 'cars' => $cars]);
+    }
 }

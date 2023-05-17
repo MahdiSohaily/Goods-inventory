bbe59085b174254ed44efe87ce459cacdadfe904
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-17 15:08:12 +0330

    Update the store and update sql comment

--- resources/js/Pages/Rates/Show.vue
+++ resources/js/Pages/Rates/Show.vue
@@ -1,6 +1,11 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
-
+const remove = (id) => {
+   
+   if (confirm("Are you sure you want to delete this record?") == true) {
+       router.delete(route("goods.delete", id));
+   }
+};
 defineProps({
     rates: Object,
 });

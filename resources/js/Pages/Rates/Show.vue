05056d571615c6a790d23c5cf94ca33709b02b36
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-17 15:12:46 +0330

    Update the store and update sql comment

--- resources/js/Pages/Rates/Show.vue
+++ resources/js/Pages/Rates/Show.vue
@@ -3,7 +3,7 @@ import AppLayout from "@/Layouts/AppLayout.vue";
 const remove = (id) => {
    
    if (confirm("Are you sure you want to delete this record?") == true) {
-       router.delete(route("goods.delete", id));
+       router.delete(route("rates.delete", id));
    }
 };
 defineProps({

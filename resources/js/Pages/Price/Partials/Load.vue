378c0c39f183b896e4568e0849cbc9bf0e2023c0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-22 11:39:24 +0330

    Add the load page for the price load

--- resources/js/Pages/Price/Partials/Load.vue
+++ resources/js/Pages/Price/Partials/Load.vue
@@ -34,14 +34,14 @@ const LoadPrice = () => {
 <template>
     <AppLayout title="Rates">
         <div class="h-70S grid grid-cols-1 my-8 md:grid-cols-9 gap-6 lg:gap-2 lg:p-2">
-            <div class="bg-white rounded-lg shadow-md py-12">
+            <div class="bg-white rounded-lg shadow-md">
                 <div class="flex items-center justify-between p-3">
                     <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2">
                         {{ code }}
                     </h2>
                 </div>
                 <div id="search_result" class="p-3">
-                    {{ pattern.name }}
+                    <p class="text-center">{{ pattern.name }}</p>
                     <ul>
                         <li v-for="item in cars">{{ item.name }}</li>
                     </ul>
@@ -49,8 +49,7 @@ const LoadPrice = () => {
             </div>
             <div class="bg-white rounded-lg shadow-md col-span-4">
                 <div class="flex items-center justify-between p-3">
-                    <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2">
-                    </h2>
+                    <h2 class="text-xl font-semibold text-gray-800 flex items-center gap-2"></h2>
                 </div>
                 <SectionBorder />
                 <div id="search_result" class="p-3">

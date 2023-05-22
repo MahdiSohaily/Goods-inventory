3b10ef7c48c654c5d08b728e98b783345a4ea3d3
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-22 13:58:07 +0330

    Add customers table migration file

--- database/migrations/2023_05_22_101828_create_customers_table.php
+++ database/migrations/2023_05_22_101828_create_customers_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('last_name');
             $table->timestamps();
         });
     }

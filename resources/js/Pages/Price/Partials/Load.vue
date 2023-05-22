ce348dbbe359621c4cbb06ac6738cdb7b4deb691
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-22 12:36:20 +0330

    Add the load page for the price load

--- resources/js/Pages/Price/Partials/Load.vue
+++ resources/js/Pages/Price/Partials/Load.vue
@@ -36,18 +36,15 @@ const LoadPrice = () => {
 
 <template>
     <AppLayout title="Rates">
-        <div
-            class="h-70S grid grid-cols-1 my-8 md:grid-cols-7 gap-6 lg:gap-2 lg:p-2"
-        >
-            <div class="bg-white rounded-lg shadow-md">
+        <div class="pt-9">
+            <div class="bg-white rounded-lg shadow-md max-w-2xl m-auto">
                 <div class="flex items-center justify-between">
-                    <h2
-                        class="text-xl font-semibold bg-violet-600 w-full rounded-t-md p-3 text-center text-white"
-                    >
-                        {{ code }}
+                    <h2 class="text-xl font-semibold bg-violet-600 w-full rounded-t-md p-3 text-center text-white">
+                       Selected Part Number
                     </h2>
                 </div>
                 <div id="search_result" class="p-3">
+                    <p class="text-center">{{ code }}</p>
                     <p class="text-center">{{ pattern.name }}</p>
                     <ul>
                         <li class="text-center" v-for="item in cars">
@@ -55,12 +52,12 @@ const LoadPrice = () => {
                         </li>
                     </ul>
                 </div>
-            </div>
-            <div class="bg-white rounded-lg shadow-md col-span-4">
+        </div>
+        </div>
+        <div class="h-70S grid grid-cols-1 md:grid-cols-4 gap-6 lg:gap-2 lg:p-2">         
+            <div class="bg-white rounded-lg shadow-md col-span-2">
                 <div class="flex items-center justify-between">
-                    <h2
-                        class="text-xl font-semibold bg-violet-600 w-full rounded-t-md p-3 text-center text-white"
-                    >
+                    <h2 class="text-xl font-semibold bg-violet-600 w-full rounded-t-md p-3 text-center text-white">
                         Related Parts
                     </h2>
                 </div>
@@ -93,31 +90,24 @@ const LoadPrice = () => {
                                     </ul>
                                 </td>
                                 <td>
-                                    <table
-                                        class="min-w-full text-left text-sm font-light"
-                                    >
+                                    <table class="min-w-full text-left text-sm font-light">
                                         <thead class="font-medium">
                                             <tr>
-                                                <th
-                                                    v-for="rate in selected_rates"
-                                                    scope="col"
-                                                    class="text-gray-800 text-center"
-                                                >
+                                                <th v-for="rate in selected_rates" scope="col"
+                                                    class="text-gray-800 text-center">
                                                     {{ rate }}
                                                 </th>
                                             </tr>
                                         </thead>
                                         <tbody id="results">
                                             <tr>
-                                                <td
-                                                    class="whitespace-nowrap px-3 py-3 text-center"
-                                                    v-for="rate in selected_rates"
-                                                >
+                                                <td class="whitespace-nowrap px-3 py-3 text-center"
+                                                    v-for="rate in selected_rates">
                                                     {{
                                                         Math.round(
                                                             (relation.price *
                                                                 110) /
-                                                                243.5
+                                                            243.5
                                                         ) *
                                                         rate *
                                                         1.2 *
@@ -127,15 +117,13 @@ const LoadPrice = () => {
                                                 </td>
                                             </tr>
                                             <tr v-if="relation.mobis">
-                                                <td
-                                                    class="whitespace-nowrap px-3 text-center"
-                                                    v-for="rate in selected_rates"
-                                                >
+                                                <td class="whitespace-nowrap px-3 text-center"
+                                                    v-for="rate in selected_rates">
                                                     {{
                                                         Math.round(
                                                             (relation.price *
                                                                 110) /
-                                                                243.5
+                                                            243.5
                                                         ) *
                                                         rate *
                                                         1.25 *
@@ -144,15 +132,13 @@ const LoadPrice = () => {
                                                 </td>
                                             </tr>
                                             <tr v-if="relation.korea">
-                                                <td
-                                                    class="whitespace-nowrap px-3 text-center"
-                                                    v-for="rate in selected_rates"
-                                                >
+                                                <td class="whitespace-nowrap px-3 text-center"
+                                                    v-for="rate in selected_rates">
                                                     {{
                                                         Math.round(
                                                             (relation.price *
                                                                 110) /
-                                                                243.5
+                                                            243.5
                                                         ) *
                                                         rate *
                                                         1.25 *
@@ -168,20 +154,88 @@ const LoadPrice = () => {
                     </table>
                 </div>
             </div>
-            <div class="bg-white rounded-lg shadow-md col-span-2">
-                <div class="flex items-center justify-between p-3">
-                    <h2
-                        class="text-xl font-semibold text-gray-800 flex items-center gap-2"
-                    >
-                        <i class="material-icons font-semibold text-orange-400"
-                            >search</i
-                        >
-                        Search Goods
+            <div class="bg-white rounded-lg shadow-md ">
+                <div class="flex items-center justify-between">
+                    <h2 class="text-xl font-semibold bg-violet-600 w-full rounded-t-md p-3 text-center text-white">
+                        Related Parts
                     </h2>
                 </div>
                 <SectionBorder />
                 <div id="search_result" class="p-3">
-                    <!-- Search Results are going to be appended here -->
+                    <table class="min-w-full text-left text-sm font-light">
+                        <thead class="font-medium bg-green-600">
+                            <tr>
+                                <th scope="col" class="px-3 text-gray-800 py-3 break-words w-36">
+                                    Part Number
+                                </th>
+                                <th scope="col" class="px-3 text-gray-800 py-3">
+                                    Goods Amount
+                                </th>
+                                <th scope="col" class="px-3 text-gray-800 py-3">
+                                    Price
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody id="results">
+                            <tr class="bg-gray-200" v-for="relation in relations">
+                                <td class="px-3">
+                                    {{ relation.partnumber }}
+                                </td>
+                                <td>
+                                    <ul>
+                                        <li v-for="item in relations">
+                                            {{ item }}
+                                        </li>
+                                    </ul>
+                                </td>
+                                <td>
+                                   g
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="bg-white rounded-lg shadow-md ">
+                <div class="flex items-center justify-between">
+                    <h2 class="text-xl font-semibold bg-violet-600 w-full rounded-t-md p-3 text-center text-white">
+                        Related Parts
+                    </h2>
+                </div>
+                <SectionBorder />
+                <div id="search_result" class="p-3">
+                    <table class="min-w-full text-left text-sm font-light">
+                        <thead class="font-medium bg-green-600">
+                            <tr>
+                                <th scope="col" class="px-3 text-gray-800 py-3 break-words w-36">
+                                    Part Number
+                                </th>
+                                <th scope="col" class="px-3 text-gray-800 py-3">
+                                    Goods Amount
+                                </th>
+                                <th scope="col" class="px-3 text-gray-800 py-3">
+                                    Price
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody id="results">
+                            <tr class="bg-gray-200" v-for="relation in relations">
+                                <td class="px-3">
+                                    {{ relation.partnumber }}
+                                </td>
+                                <td>
+                                    <ul>
+                                        <li v-for="item in relations">
+                                            {{ item }}
+                                        </li>
+                                    </ul>
+                                </td>
+                                <td>
+                                   g
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>

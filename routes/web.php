0552c6dfb994d81ed80ebbd984d2f7dbb5725318
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-22 16:42:32 +0330

    Update the grid layout for the load price

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ Route::middleware([
     // Rate Page related routs
     Route::controller(PriceController::class)->group(function () {
         Route::get('/price', 'index')->name('price.give'); // price route lead us to the price page
-        Route::get('/price/test', 'create')->name('price.give'); // price route lead us to the price page
+        Route::get('/price/test', 'create')->name('price.test'); // price route lead us to the price page
         Route::get('/price/load', 'load')->name('price.load'); // Route to create new good in the system
         Route::post('/price', 'store')->name('price.store'); // Route to create new good in the system
         Route::post('/price/pattern', 'pattern')->name('price.pattern'); // Route to create new good in the system

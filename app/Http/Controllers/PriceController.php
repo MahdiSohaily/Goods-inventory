02c5b6b17cd2c672c250ee6f4d6c45cce9806645
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-05-29 12:39:59 +0330

    Add regex to remove not number characters

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -45,79 +45,48 @@ class PriceController extends Controller
 
         return Inertia::render('Price/Partials/Load', ['allCodeData' => $allCodeData, 'customer' => $customer, 'completeCode' => $completeCode]);
     }
-
-    public function store(Request $request)
+    public function get(Type $var = null)
     {
-        Validator::make($request->all(), [
-            'partnumber' => 'required|string|exists:nisha,partnumber'
-
-        ], [
-            'required' => "The :attribute field can't be empty.",
-        ])->validate();
-
-        DB::table('prices')->insert([
-            'partnumber' => $request->input('partnumber'),
-            'price' => $request->input('price'),
-            'customer_id' => $request->input('customer'),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
-
-        $customer = $request->input('customer');
-        $completeCode = $request->input('code');
-
-        $codes = explode("\n", $request->input('code'));
-        $allCodeData = [];
-
-        foreach ($codes as $key => $value) {
-            $good = DB::table('nisha')->where('partNumber', $value)->first();
-            if ($good) {
-                array_push($allCodeData, ['result' => $this->getCodeData($good->id, $customer, $value), 'search' => $value]);
-            } else {
-                array_push($allCodeData, ['result' => null, 'search' => $value]);
-            }
-        }
-
-        return Inertia::render('Price/Partials/Load', ['allCodeData' => $allCodeData, 'customer' => $customer, 'completeCode' => $completeCode]);
+        # code...
     }
 
     public function getCodeData($code, $customer, $search)
     {
+        $good = DB::table('nisha')->where('id', $code)->first();
 
         $pattern_id = DB::table('similars')->where('nisha_id', $code)->first();
 
-            $pattern = $pattern_id ? DB::table('patterns')->where('id', $pattern_id->pattern_id)->first() : null;
+        $pattern = $pattern_id ? DB::table('patterns')->where('id', $pattern_id->pattern_id)->first() : null;
 
-            $all_relations = DB::table('similars')
-                ->join('nisha', 'similars.nisha_id', '=', 'nisha.id')
-                ->where('pattern_id', $pattern_id->pattern_id)->get();
+        $all_relations =  $pattern_id ? DB::table('similars')
+            ->join('nisha', 'similars.nisha_id', '=', 'nisha.id')
+            ->where('pattern_id', $pattern_id->pattern_id)->get() : [$good];
 
-            $cars = DB::table('patterncars')
-                ->join('cars', 'patterncars.car_id', '=', 'cars.id')
-                ->where('patterncars.pattern_id', $pattern_id->pattern_id)->get();
+        $cars = $pattern_id ? DB::table('patterncars')
+            ->join('cars', 'patterncars.car_id', '=', 'cars.id')
+            ->where('patterncars.pattern_id', $pattern_id->pattern_id)->get() : null;
 
-            $rates = DB::table('rates')
-                ->orderBy('amount', 'asc')
-                ->get();
+        $rates = DB::table('rates')
+            ->orderBy('amount', 'asc')
+            ->get();
 
-            $good = DB::table('nisha')->where('id', $code)->first();
-            $partNumber = substr($good->partnumber, 0, 7);
 
-            $prices = DB::table('prices')
-                ->select('prices.*', 'customers.name', 'customers.last_name')
-                ->join('customers', 'prices.customer_id', 'customers.id')
-                ->where('prices.partnumber', 'like', "$partNumber%")
-                ->orderBy('prices.created_at', 'desc')
-                ->limit(4)
-                ->get();
-            $existing = $this->exist($code);
-            return  [
-                'search' => $search, 'code' => $code, 'pattern' => $good->partnumber,
-                'relations' => $all_relations, 'customer' => $customer, 'cars' => $cars,
-                'rates' => $rates, 'prices' => $prices, 'name' => $pattern->name,
-                'existing' => $existing
-            ];
-        
+        $partNumber = substr($good->partnumber, 0, 7);
+
+        $prices = DB::table('prices')
+            ->select('prices.*', 'customers.name', 'customers.last_name')
+            ->join('customers', 'prices.customer_id', 'customers.id')
+            ->where('prices.partnumber', 'like', "$partNumber%")
+            ->orderBy('prices.created_at', 'desc')
+            ->limit(4)
+            ->get();
+        $existing = $this->exist($code);
+        return  [
+            'search' => $search, 'code' => $code, 'pattern' => $good->partnumber,
+            'relations' => $all_relations, 'customer' => $customer, 'cars' => $cars,
+            'rates' => $rates, 'prices' => $prices, 'name' => $pattern ? $pattern->name : 'Not in relation',
+            'existing' => $existing
+        ];
     }
 
     public function out($id)
@@ -161,5 +130,40 @@ class PriceController extends Controller
         }
 
         return [$amount, $brands];
+    }
+
+    public function store(Request $request)
+    {
+        Validator::make($request->all(), [
+            'partnumber' => 'required|string|exists:nisha,partnumber'
+
+        ], [
+            'required' => "The :attribute field can't be empty.",
+        ])->validate();
+
+        DB::table('prices')->insert([
+            'partnumber' => $request->input('partnumber'),
+            'price' => $request->input('price'),
+            'customer_id' => $request->input('customer'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        $customer = $request->input('customer');
+        $completeCode = $request->input('code');
+
+        $codes = explode("\n", $request->input('code'));
+        $allCodeData = [];
+
+        foreach ($codes as $key => $value) {
+            $good = DB::table('nisha')->where('partNumber', $value)->first();
+            if ($good) {
+                array_push($allCodeData, ['result' => $this->getCodeData($good->id, $customer, $value), 'search' => $value]);
+            } else {
+                array_push($allCodeData, ['result' => null, 'search' => $value]);
+            }
+        }
+
+        return Inertia::render('Price/Partials/Load', ['allCodeData' => $allCodeData, 'customer' => $customer, 'completeCode' => $completeCode]);
     }
 }

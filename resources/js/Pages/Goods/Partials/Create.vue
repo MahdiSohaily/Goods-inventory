fa762ce1c1a7dd9995815379f3a62f6038d529e5
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-06 11:38:39 +0330

    Update the update page form labels and instructions

--- resources/js/Pages/Goods/Partials/Create.vue
+++ resources/js/Pages/Goods/Partials/Create.vue
@@ -36,112 +36,61 @@ const clearInput = () => {
 <template>
     <AppLayout title="Goods">
         <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="rtl max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
                 <FormSection @submitted="createGood">
-                    <template #title> Create New Good </template>
+                    <template #title> ثبت جنس جدید در سیستم </template>
 
                     <template #description>
-                        Fill the following form fields carefully in order to
-                        register new good in the system.
+                        برای ثبت یک جنس جدید در سیستم فورم ذیل را به صورت دقیق خانه پری نمایید.
                     </template>
 
                     <template #form>
                         <!-- Serial Number -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="serial" value="Part Number" />
-                            <TextInput
-                                required
-                                id="serial"
-                                v-model="form.serial"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="serial"
-                            />
-                            <InputError
-                                :message="form.errors.serial"
-                                class="mt-2"
-                            />
+                            <InputLabel for="serial" value="شماره فنی" />
+                            <TextInput required id="serial" v-model="form.serial" type="text" class="mt-1 block w-full"
+                                autocomplete="serial" />
+                            <InputError :message="form.errors.serial" class="mt-2" />
                         </div>
 
                         <!-- Price -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="price" value="Price" />
-                            <TextInput
-                                required
-                                id="price"
-                                v-model="form.price"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="username"
-                            />
-                            <InputError
-                                :message="form.errors.price"
-                                class="mt-2"
-                            />
+                            <InputLabel for="price" value="قیمت جنس" />
+                            <TextInput required id="price" v-model="form.price" type="text" class="mt-1 block w-full"
+                                autocomplete="username" />
+                            <InputError :message="form.errors.price" class="mt-2" />
                         </div>
                         <!-- Weight -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="weight" value="Weight" />
-                            <TextInput
-                                required
-                                id="weight"
-                                v-model="form.weight"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="username"
-                            />
-                            <InputError
-                                :message="form.errors.weight"
-                                class="mt-2"
-                            />
+                            <InputLabel for="weight" value="وزن جنس" />
+                            <TextInput required id="weight" v-model="form.weight" type="text" class="mt-1 block w-full"
+                                autocomplete="username" />
+                            <InputError :message="form.errors.weight" class="mt-2" />
                         </div>
                         <!-- Mobis -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="mobis" value="Mobis" />
-                            <TextInput
-                                required
-                                id="mobis"
-                                v-model="form.mobis"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="username"
-                            />
-                            <InputError
-                                :message="form.errors.mobis"
-                                class="mt-2"
-                            />
+                            <InputLabel for="mobis" value="موبیز" />
+                            <TextInput required id="mobis" v-model="form.mobis" type="text" class="mt-1 block w-full"
+                                autocomplete="username" />
+                            <InputError :message="form.errors.mobis" class="mt-2" />
                         </div>
                         <!-- Korea section -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="korea" value="Korea" />
-                            <TextInput
-                                required
-                                id="korea"
-                                v-model="form.korea"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="username"
-                            />
-                            <InputError
-                                :message="form.errors.korea"
-                                class="mt-2"
-                            />
+                            <InputLabel for="korea" value="کوریا" />
+                            <TextInput required id="korea" v-model="form.korea" type="text" class="mt-1 block w-full"
+                                autocomplete="username" />
+                            <InputError :message="form.errors.korea" class="mt-2" />
                         </div>
                     </template>
 
                     <template #actions>
-                        <ActionMessage
-                            :on="form.recentlySuccessful"
-                            class="mr-3"
-                        >
-                            Saved.
+                        <ActionMessage :on="form.recentlySuccessful" class="mr-3">
+                            ذخیره سازی موفقانه صورت گرفت.
                         </ActionMessage>
 
-                        <PrimaryButton
-                            :class="{ 'opacity-25': form.processing }"
-                            :disabled="form.processing"
-                        >
-                            Save
+                        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            <i class="px-2 material-icons hover:cursor-pointer">save</i>
+                            ذخیره سازی
                         </PrimaryButton>
                     </template>
                 </FormSection>
--- resources/js/Pages/Goods/Partials/Update.vue
+++ resources/js/Pages/Goods/Partials/Update.vue
@@ -32,112 +32,61 @@ const updateGood = () => {
 <template>
     <AppLayout title="Goods">
         <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="rtl max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
                 <FormSection @submitted="updateGood">
-                    <template #title> Update Good </template>
+                    <template #title> ویرایش جنس </template>
 
                     <template #description>
-                        Fill the following form fields carefully in order to
-                        update good in the system.
+                        برای ویرایش جنس انخاب شده مشخصات ذیل را به دقت کامل ویرایش نمایید.
                     </template>
 
                     <template #form>
                         <!-- Serial Number -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="serial" value="Part Number" />
-                            <TextInput
-                                required
-                                id="serial"
-                                v-model="form.serial"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="serial"
-                            />
-                            <InputError
-                                :message="form.errors.serial"
-                                class="mt-2"
-                            />
+                            <InputLabel for="serial" value="شماره فنی" />
+                            <TextInput required id="serial" v-model="form.serial" type="text" class="mt-1 block w-full"
+                                autocomplete="serial" />
+                            <InputError :message="form.errors.serial" class="mt-2" />
                         </div>
 
                         <!-- Price -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="price" value="Price" />
-                            <TextInput
-                                required
-                                id="price"
-                                v-model="form.price"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="username"
-                            />
-                            <InputError
-                                :message="form.errors.price"
-                                class="mt-2"
-                            />
+                            <InputLabel for="price" value="قیمت جنس" />
+                            <TextInput required id="price" v-model="form.price" type="text" class="mt-1 block w-full"
+                                autocomplete="username" />
+                            <InputError :message="form.errors.price" class="mt-2" />
                         </div>
                         <!-- Weight -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="weight" value="Weight" />
-                            <TextInput
-                                required
-                                id="weight"
-                                v-model="form.weight"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="username"
-                            />
-                            <InputError
-                                :message="form.errors.weight"
-                                class="mt-2"
-                            />
+                            <InputLabel for="weight" value="وزن جنس" />
+                            <TextInput required id="weight" v-model="form.weight" type="text" class="mt-1 block w-full"
+                                autocomplete="username" />
+                            <InputError :message="form.errors.weight" class="mt-2" />
                         </div>
                         <!-- Mobis -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="mobis" value="Mobis" />
-                            <TextInput
-                                required
-                                id="mobis"
-                                v-model="form.mobis"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="username"
-                            />
-                            <InputError
-                                :message="form.errors.mobis"
-                                class="mt-2"
-                            />
+                            <InputLabel for="mobis" value="موبیز" />
+                            <TextInput required id="mobis" v-model="form.mobis" type="text" class="mt-1 block w-full"
+                                autocomplete="username" />
+                            <InputError :message="form.errors.mobis" class="mt-2" />
                         </div>
                         <!-- korea -->
                         <div class="col-span-6 sm:col-span-4">
-                            <InputLabel for="korea" value="Korea" />
-                            <TextInput
-                                required
-                                id="korea"
-                                v-model="form.korea"
-                                type="text"
-                                class="mt-1 block w-full"
-                                autocomplete="username"
-                            />
-                            <InputError
-                                :message="form.errors.korea"
-                                class="mt-2"
-                            />
+                            <InputLabel for="korea" value="کوریا" />
+                            <TextInput required id="korea" v-model="form.korea" type="text" class="mt-1 block w-full"
+                                autocomplete="username" />
+                            <InputError :message="form.errors.korea" class="mt-2" />
                         </div>
                     </template>
 
                     <template #actions>
-                        <ActionMessage
-                            :on="form.recentlySuccessful"
-                            class="mr-3"
-                        >
-                            Record Updated Successfully.
+                        <ActionMessage :on="form.recentlySuccessful" class="mr-3">
+                            عملیات ویرایش موفقانه صورت گرفت.
                         </ActionMessage>
 
-                        <PrimaryButton
-                            :class="{ 'opacity-25': form.processing }"
-                            :disabled="form.processing"
-                        >
-                            Update
+                        <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                            <i class="px-2 material-icons hover:cursor-pointer">swap_vertical_circle</i>
+                            ویرایش
                         </PrimaryButton>
                     </template>
                 </FormSection>

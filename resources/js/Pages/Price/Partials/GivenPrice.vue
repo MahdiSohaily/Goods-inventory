386723e62ccab1c0692480accee321ad00f4361e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-06 09:14:35 +0330

    Add given price component

--- resources/js/Pages/Price/Partials/GivenPrice.vue
+++ resources/js/Pages/Price/Partials/GivenPrice.vue
@@ -1,37 +1,104 @@
 <script setup>
-defineProps({
-    rates: Object,
-});
+import { useForm } from "@inertiajs/vue3";
+import ActionMessage from "@/Components/ActionMessage.vue";
+import FormRelation from "@/Components/FormRelation.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+
+const selected_rates = [50, 51, 52, 55];
 
 const form = useForm({
-    _method: "GET",
-    customer: null,
-    code: null,
+    _method: "POST",
+    customer: props.customer,
+    partnumber: null,
+    price: null,
+    code: props.completeCode,
 });
 
-const LoadPrice = () => {
-    form.post(route("price.load"), {
-        errorBag: "LoadPrice",
-        preserveScroll: true,
-        onSuccess: () => clearInput(),
-    });
+const changeVal = (value) => {
+    form.price = value;
 };
 
-const clearInput = () => { };
+const savePrice = (pattern) => {
+    form.partnumber = pattern;
+    form.post(route("price.store"), {
+        errorBag: "savePrice",
+        preserveScroll: true,
+        onSuccess: () => { },
+    });
+};
 </script>
 
 <template>
-    <div class="bg-white rounded-lg">
+    <div class="bg-white rounded-lg shadow-md col-span-2">
         <div id="search_result" class="p-3">
-            <p class="text-center bg-gray-600 text-white p-2 my-3 rounded-md">
-                {{ item.search }}
-            </p>
-            <p class="text-center my-2">{{ item.result.name }}</p>
-            <ul>
-                <li class="text-center" v-for="elem in item.result.cars">
-                    {{ elem.name }}
-                </li>
-            </ul>
+            <table class="min-w-full text-left text-sm font-light">
+                <thead>
+                    <tr>
+                        <th>
+                            <span class="px-4 text-gray-600">part NA</span>
+                            <span class="px-4 text-gray-600">Price</span>
+                            <span class="px-4 text-gray-600">Client</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <div class="min-w-full mb-1 border-2 border-gray-400" v-if="item.result.ordered_price.price != null">
+                        <div class="min-w-full bg-red-400">
+                            <td scope="col" class="px-3 text-white py-2 break-words">
+                                Ordered Price
+                            </td>
+                            <td colspan="2" scope="col" class="px-3 text-white py-2 break-words">
+                                {{ item.result.ordered_price.price }}
+                            </td>
+                        </div>
+                    </div>
+                    <div class="min-w-full mb-1 border-2 border-gray-400" v-for="price in item.result.prices">
+                        <div class="min-w-full bg-indigo-100">
+                            <td scope="col" class="px-3 text-gray-800 py-1 break-words">
+                                {{ price.partnumber }}
+                            </td>
+                            <td scope="col" class="px-3 text-gray-800 py-1 break-words">
+                                {{ price.price }}
+                            </td>
+                            <td scope="col" class="px-3 text-gray-800 py-1">
+                                {{ price.name }}
+                            </td>
+                        </div>
+                        <div class="min-w-full bg-orange-100 py-1">
+                            <td colspan="3" scope="col" class="px-3 text-gray-800">
+                                {{ arrangeTime(price.created_at) }}
+                            </td>
+                        </div>
+                    </div>
+                </tbody>
+            </table>
+            <FormRelation @submitted="savePrice(item.result.pattern)">
+                <template #form>
+                    <!-- Name -->
+                    <div class="pb-2">
+                        <InputLabel for="price" value="Price" />
+                        <TextInput id="price" @keyup="
+                            (e) => {
+                                changeVal(e.target.value);
+                            }
+                        " type="text" class="mt-1 block w-full" autocomplete="price" />
+                        <InputError :message="form.errors.price" class="mt-2" />
+                    </div>
+                </template>
+
+                <template #actions>
+                    <ActionMessage :on="form.recentlySuccessful" class="mr-3">
+                        Saved.
+                    </ActionMessage>
+
+                    <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                        Save
+                    </PrimaryButton>
+                </template>
+            </FormRelation>
         </div>
     </div>
 </template>
--- resources/js/Pages/Price/Partials/Load.vue
+++ resources/js/Pages/Price/Partials/Load.vue
@@ -1,14 +1,8 @@
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
-import { useForm } from "@inertiajs/vue3";
-import ActionMessage from "@/Components/ActionMessage.vue";
-import FormRelation from "@/Components/FormRelation.vue";
-import InputError from "@/Components/InputError.vue";
-import InputLabel from "@/Components/InputLabel.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import TextInput from "@/Components/TextInput.vue";
 import CodeInfo from "./CodeInfo.vue";
 import RelationsDelatils from "./RelationsDelatils.vue";
+import GivenPrice from "./GivenPrice.vue";
 
 const props = defineProps({
   allCodeData: Array,
@@ -17,28 +11,7 @@ const props = defineProps({
 });
 
 
-const selected_rates = [50, 51, 52, 55];
 
-const form = useForm({
-  _method: "POST",
-  customer: props.customer,
-  partnumber: null,
-  price: null,
-  code: props.completeCode,
-});
-
-const changeVal = (value) => {
-  form.price = value;
-};
-
-const savePrice = (pattern) => {
-  form.partnumber = pattern;
-  form.post(route("price.store"), {
-    errorBag: "savePrice",
-    preserveScroll: true,
-    onSuccess: () => { },
-  });
-};
 const arrangeTime = (dateTime) => {
   const now = new Date();
   const create = new Date(dateTime);
@@ -85,76 +58,7 @@ const arrangeTime = (dateTime) => {
       <div v-if="null != item.result" class="grid grid-cols-1 md:grid-cols-10 gap-6 lg:gap-2 lg:p-2">
         <CodeInfo />
         <RelationsDelatils />
-        <
-        <div class="bg-white rounded-lg shadow-md col-span-2">
-          <div id="search_result" class="p-3">
-            <table class="min-w-full text-left text-sm font-light">
-              <thead>
-                <tr>
-                  <th>
-                    <span class="px-4 text-gray-600">part NA</span>
-                    <span class="px-4 text-gray-600">Price</span>
-                    <span class="px-4 text-gray-600">Client</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <div class="min-w-full mb-1 border-2 border-gray-400" v-if="item.result.ordered_price.price != null">
-                  <div class="min-w-full bg-red-400">
-                    <td scope="col" class="px-3 text-white py-2 break-words">
-                      Ordered Price
-                    </td>
-                    <td colspan="2" scope="col" class="px-3 text-white py-2 break-words">
-                      {{ item.result.ordered_price.price }}
-                    </td>
-                  </div>
-                </div>
-                <div class="min-w-full mb-1 border-2 border-gray-400" v-for="price in item.result.prices">
-                  <div class="min-w-full bg-indigo-100">
-                    <td scope="col" class="px-3 text-gray-800 py-1 break-words">
-                      {{ price.partnumber }}
-                    </td>
-                    <td scope="col" class="px-3 text-gray-800 py-1 break-words">
-                      {{ price.price }}
-                    </td>
-                    <td scope="col" class="px-3 text-gray-800 py-1">
-                      {{ price.name }}
-                    </td>
-                  </div>
-                  <div class="min-w-full bg-orange-100 py-1">
-                    <td colspan="3" scope="col" class="px-3 text-gray-800">
-                      {{ arrangeTime(price.created_at) }}
-                    </td>
-                  </div>
-                </div>
-              </tbody>
-            </table>
-            <FormRelation @submitted="savePrice(item.result.pattern)">
-              <template #form>
-                <!-- Name -->
-                <div class="pb-2">
-                  <InputLabel for="price" value="Price" />
-                  <TextInput id="price" @keyup="
-                    (e) => {
-                      changeVal(e.target.value);
-                    }
-                  " type="text" class="mt-1 block w-full" autocomplete="price" />
-                  <InputError :message="form.errors.price" class="mt-2" />
-                </div>
-              </template>
-
-              <template #actions>
-                <ActionMessage :on="form.recentlySuccessful" class="mr-3">
-                  Saved.
-                </ActionMessage>
-
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                  Save
-                </PrimaryButton>
-              </template>
-            </FormRelation>
-          </div>
-        </div>
+        <GivenPrice />
         <div class="bg-white rounded-lg shadow-md col-span-2">
           <div class="p-3">
             <table class="min-w-full text-left text-sm font-light">

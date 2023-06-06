a04fa695267056c0d7d6e50ff2568533e25014e2
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-06 09:09:35 +0330

    Add relation details component

--- resources/js/Pages/Price/Partials/Load.vue
+++ resources/js/Pages/Price/Partials/Load.vue
@@ -7,6 +7,7 @@ import InputError from "@/Components/InputError.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import TextInput from "@/Components/TextInput.vue";
+import CodeInfo from "./CodeInfo.vue";
 
 const props = defineProps({
   allCodeData: Array,
@@ -81,106 +82,8 @@ const arrangeTime = (dateTime) => {
     {{ sortable }}
     <div v-for="item in allCodeData">
       <div v-if="null != item.result" class="grid grid-cols-1 md:grid-cols-10 gap-6 lg:gap-2 lg:p-2">
-       
-        <div class="bg-white rounded-lg col-span-5 scroll-auto">
-          <div id="search_result" class="p-3">
-            <table class="min-w-full text-left text-sm font-light">
-              <thead class="font-medium bg-green-300">
-                <tr>
-                  <th scope="col" class="px-3 text-center text-gray-800 py-3">
-                    Part Number
-                  </th>
-                  <th scope="col" class="px-3 text-center text-gray-800 py-3">
-                    Goods Amount
-                  </th>
-                  <th scope="col" class="px-3 text-center text-gray-800 py-3">
-                    Price
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="element, key in item.result.sorted">
-                  <td class="px-1 pt-2">
-                    <p class="bold">
-                      {{ item.result.relations[key.replace('z','')].partnumber }}
-                    </p>
-                  </td>
-                  <td class="px-1 pt-2">
-                    <table class="min-w-full text-left text-sm font-light bg-gray-200 p-2 border-2 border-gray-400">
-                      <thead class="font-medium">
-                        <tr>
-                          <th v-for="rate, index in item.result.existing[item.result.relations[key.replace('z','')].id]" scope="col"
-                            class="text-gray-800 text-center bg-orange-200 py-2">
-                            {{ index }}
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr class="bg-violet-200 py-3">
-                          <td class="whitespace-nowrap px-3 py-2 text-center"
-                            v-for="rate, index in item.result.existing[item.result.relations[key.replace('z','')].id]">
-                            {{ rate }}
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </td>
-                  <td class="px-1 pt-2">
-                    <table class="min-w-full text-left text-sm font-light bg-gray-200 p-2 border-2 border-gray-400">
-                      <thead class="font-medium">
-                        <tr>
-                          <th v-for="rate in selected_rates" scope="col"
-                            class="text-gray-800 text-center bg-orange-200 py-2">
-                            {{ rate }}
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr class="bg-violet-200 py-3">
-                          <td class="whitespace-nowrap px-3 py-2 text-center" v-for="rate in selected_rates">
-                            {{
-                              Math.round(
-                                Math.round((item.result.relations[key.replace('z','')].price * 110) / 243.5) *
-                                rate *
-                                1.2 *
-                                1.2 *
-                                1.3
-                              )
-                            }}
-                          </td>
-                        </tr>
-                        <tr class="bg-indigo-200" v-if="
-                          item.result.relations[key.replace('z','')].mobis !== null && item.result.relations[key.replace('z','')].mobis !== '-'
-                        ">
-                          <td class="whitespace-nowrap px-3 text-center py-2" v-for="rate in selected_rates">
-                            {{
-                              Math.round(
-                                Math.round((item.result.relations[key.replace('z','')].price * 110) / 243.5) *
-                                rate *
-                                1.25 *
-                                1.3
-                              )
-                            }}
-                          </td>
-                        </tr>
-                        <tr v-if="item.result.relations[key.replace('z','')].korea">
-                          <td class="whitespace-nowrap px-3 text-center" v-for="rate in selected_rates">
-                            {{
-                              Math.round((item.result.relations[key.replace('z','')].price * 110) / 243.5) *
-                              rate *
-                              1.25 *
-                              1.3
-                            }}
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+       <CodeInfo />
+     
         <div class="bg-white rounded-lg shadow-md col-span-2">
           <div id="search_result" class="p-3">
             <table class="min-w-full text-left text-sm font-light">
---
+++ resources/js/Pages/Price/Partials/RelationsDetails.vue
@@ -0,0 +1,123 @@
+<script setup>
+defineProps({
+    rates: Object,
+});
+
+const form = useForm({
+    _method: "GET",
+    customer: null,
+    code: null,
+});
+
+const LoadPrice = () => {
+    form.post(route("price.load"), {
+        errorBag: "LoadPrice",
+        preserveScroll: true,
+        onSuccess: () => clearInput(),
+    });
+};
+
+const clearInput = () => { };
+</script>
+
+<template>
+    <div class="bg-white rounded-lg col-span-5 scroll-auto">
+        <div id="search_result" class="p-3">
+            <table class="min-w-full text-left text-sm font-light">
+                <thead class="font-medium bg-green-300">
+                    <tr>
+                        <th scope="col" class="px-3 text-center text-gray-800 py-3">
+                            Part Number
+                        </th>
+                        <th scope="col" class="px-3 text-center text-gray-800 py-3">
+                            Goods Amount
+                        </th>
+                        <th scope="col" class="px-3 text-center text-gray-800 py-3">
+                            Price
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="element, key in item.result.sorted">
+                        <td class="px-1 pt-2">
+                            <p class="bold">
+                                {{ item.result.relations[key.replace('z', '')].partnumber }}
+                            </p>
+                        </td>
+                        <td class="px-1 pt-2">
+                            <table class="min-w-full text-left text-sm font-light bg-gray-200 p-2 border-2 border-gray-400">
+                                <thead class="font-medium">
+                                    <tr>
+                                        <th v-for="rate, index in item.result.existing[item.result.relations[key.replace('z', '')].id]"
+                                            scope="col" class="text-gray-800 text-center bg-orange-200 py-2">
+                                            {{ index }}
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-violet-200 py-3">
+                                        <td class="whitespace-nowrap px-3 py-2 text-center"
+                                            v-for="rate, index in item.result.existing[item.result.relations[key.replace('z', '')].id]">
+                                            {{ rate }}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                        <td class="px-1 pt-2">
+                            <table class="min-w-full text-left text-sm font-light bg-gray-200 p-2 border-2 border-gray-400">
+                                <thead class="font-medium">
+                                    <tr>
+                                        <th v-for="rate in selected_rates" scope="col"
+                                            class="text-gray-800 text-center bg-orange-200 py-2">
+                                            {{ rate }}
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-violet-200 py-3">
+                                        <td class="whitespace-nowrap px-3 py-2 text-center" v-for="rate in selected_rates">
+                                            {{
+                                                Math.round(
+                                                    Math.round((item.result.relations[key.replace('z', '')].price * 110) / 243.5) *
+                                                    rate *
+                                                    1.2 *
+                                                    1.2 *
+                                                    1.3
+                                                )
+                                            }}
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-indigo-200" v-if="
+                                        item.result.relations[key.replace('z', '')].mobis !== null && item.result.relations[key.replace('z', '')].mobis !== '-'
+                                    ">
+                                        <td class="whitespace-nowrap px-3 text-center py-2" v-for="rate in selected_rates">
+                                            {{
+                                                Math.round(
+                                                    Math.round((item.result.relations[key.replace('z', '')].price * 110) / 243.5) *
+                                                    rate *
+                                                    1.25 *
+                                                    1.3
+                                                )
+                                            }}
+                                        </td>
+                                    </tr>
+                                    <tr v-if="item.result.relations[key.replace('z', '')].korea">
+                                        <td class="whitespace-nowrap px-3 text-center" v-for="rate in selected_rates">
+                                            {{
+                                                Math.round((item.result.relations[key.replace('z', '')].price * 110) / 243.5) *
+                                                rate *
+                                                1.25 *
+                                                1.3
+                                            }}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>

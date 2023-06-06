faa0c453538bc944f8e0861d47ce9b051037dd3e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-06 10:02:29 +0330

    Update get codes logic

--- resources/js/Pages/Rates/Partials/Create.vue
+++ resources/js/Pages/Rates/Partials/Create.vue
@@ -11,6 +11,7 @@ import TextInput from "@/Components/TextInput.vue";
 const form = useForm({
     _method: "POST",
     amount: null,
+    selected: false,
     status: 'N',
 });
 
@@ -23,8 +24,8 @@ const createRate = () => {
 };
 
 const clearInput = () => {
-        amount.value.value = null;
-        status.value.value = null;
+    form.amount = null;
+    form.status = null;
 };
 </script>
 
@@ -43,29 +44,17 @@ const clearInput = () => {
                     <!-- Amount -->
                     <div class="col-span-6 sm:col-span-4">
                         <InputLabel for="amount" value="Amount" />
-                        <TextInput
-                            id="amount"
-                            v-model="form.amount"
-                            type="text"
-                            class="mt-1 block w-full"
-                            autocomplete="amount"
-                        />
-                        <InputError
-                            :message="form.errors.amount"
-                            class="mt-2"
-                        />
+                        <TextInput id="amount" v-model="form.amount" type="text" class="mt-1 block w-full"
+                            autocomplete="amount" />
+                        <InputError :message="form.errors.amount" class="mt-2" />
                     </div>
 
                     <!-- status -->
                     <div class="col-span-6 sm:col-span-4">
                         <InputLabel for="status" value="Status" />
-                        <select
-                            type="status"
+                        <select type="status"
                             class="mt-1 block w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-                            autocomplete="username"
-                            v-model="form.status"
-                            id="status"
-                        >
+                            autocomplete="username" v-model="form.status" id="status">
                             <option value="A" class="A">A</option>
                             <option value="B" class="B">B</option>
                             <option value="C" class="C">C</option>
@@ -75,10 +64,13 @@ const clearInput = () => {
                             <option value="G" class="G">G</option>
                             <option value="N" class="N">N</option>
                         </select>
-                        <InputError
-                            :message="form.errors.status"
-                            class="mt-2"
-                        />
+                        <InputError :message="form.errors.status" class="mt-2" />
+                    </div>
+                    {{ form.selected }}
+                    <div class="col-span-6 sm:col-span-4 flex items-center">
+                        <input class="mx-2" type="checkbox" v-model="form.selected" name="selected" id="selected">
+                        <label for="selected" value="">Show values for this rate </label>
+                        <InputError :message="form.errors.selected" class="mt-2" />
                     </div>
                 </template>
 
@@ -87,10 +79,7 @@ const clearInput = () => {
                         Saved.
                     </ActionMessage>
 
-                    <PrimaryButton
-                        :class="{ 'opacity-25': form.processing }"
-                        :disabled="form.processing"
-                    >
+                    <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                         Save
                     </PrimaryButton>
                 </template>

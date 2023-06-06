f91a6ee17dbe6de98fe492c5680006fae1c354bd
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-06 11:25:35 +0330

    Update the goods list style

--- resources/js/Pages/Goods/Show.vue
+++ resources/js/Pages/Goods/Show.vue
@@ -19,7 +19,7 @@ const removeItem = (e) => {
 };
 
 const remove = (id) => {
-   
+
     if (confirm("Are you sure you want to delete this record?") == true) {
         router.delete(route("goods.delete", id));
     }
@@ -166,134 +166,74 @@ const print = (data) => {
 <template>
     <AppLayout title="Goods">
         <div>
-            <div
-                class="max-w-7xl overflow-x-auto mx-auto pt-11 pb-3 flex justify-between"
-            >
-                <input
-                    type="text"
-                    name="serial"
-                    id="serial"
-                    class="rounded-md py-2 w-96 border-2 bg-gray-100"
-                    min="0"
-                    max="30"
-                    @keyup="search($event.target.value, rates)"
-                    placeholder="Search base on Part Number..."
-                />
-                <a
-                    :href="route('goods.create')"
-                    class="bg-indigo-500 hover:bg-indigo-400 rounded-md text-white px-4 flex justify-center items-center"
-                    >Create New</a
-                >
+            <div class="max-w-7xl overflow-x-auto mx-auto pt-11 pb-3 flex justify-between">
+                <input type="text" name="serial" id="serial" class="rounded-md py-2 w-96 border-2 bg-gray-100" min="0"
+                    max="30" @keyup="search($event.target.value, rates)" placeholder="Search base on Part Number..." />
+                <a :href="route('goods.create')"
+                    class="bg-indigo-500 hover:bg-indigo-400 rounded-md text-white px-4 flex justify-center items-center">
+                    ثبت جنس جدید
+                    <i class="px-2 material-icons hover:cursor-pointer">add_circle_outline</i>
+                </a>
             </div>
             <div class="bg-gray-100 bg-opacity-25">
                 <div class="max-w-7xl overflow-x-auto mx-auto">
-                    <table class="min-w-full text-left text-sm font-light">
+                    <table class="rtl min-w-full text-left text-sm font-light">
                         <thead class="font-medium dark:border-neutral-500">
                             <tr class="bg-green-700">
-                                <th
-                                    scope="col"
-                                    class="px-3 py-3 text-white text-center"
-                                >
+                                <th scope="col" class="px-3 py-3 text-white text-center">
                                     شماره فنی
                                 </th>
-                                <th
-                                    scope="col"
-                                    class="px-3 py-3 text-white text-center"
-                                >
+                                <th scope="col" class="px-3 py-3 text-white text-center">
                                     قیمت
                                 </th>
-                                <th
-                                    scope="col"
-                                    class="px-3 py-3 text-white text-center"
-                                >
+                                <th scope="col" class="px-3 py-3 text-white text-center">
                                     وزن
                                 </th>
-                                <th
-                                    scope="col"
-                                    class="px-3 py-3 text-white text-center"
-                                >
+                                <th scope="col" class="px-3 py-3 text-white text-center">
                                     موبیز
                                 </th>
-                                <th
-                                    scope="col"
-                                    class="px-3 py-3 text-white text-center"
-                                >
+                                <th scope="col" class="px-3 py-3 text-white text-center">
                                     کورآ
                                 </th>
-                                <th
-                                    scope="col"
-                                    class="px-3 py-3 text-white text-center"
-                                >
+                                <th scope="col" class="px-3 py-3 text-white text-center w-24">
                                     عملیات
                                 </th>
                             </tr>
                         </thead>
-                        <tbody
-                            id="results"
-                            :data-length="Math.round(count / 10)"
-                        >
-                            <tr
-                                v-if="goods.length > 0"
-                                v-for="item in goods"
-                                class="transition duration-300 ease-in-out bg-neutral-200 hover:bg-neutral-100"
-                            >
-                                <td
-                                    class="whitespace-nowrap px-3 py-3 text-center font-bold"
-                                >
+                        <tbody id="results" :data-length="Math.round(count / 10)">
+                            <tr v-if="goods.length > 0" v-for="item in goods"
+                                class="transition duration-300 ease-in-out bg-neutral-200 hover:bg-neutral-100">
+                                <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
                                     {{ item.partnumber }}
                                 </td>
-                                <td
-                                    class="whitespace-nowrap px-3 py-3 text-center font-bold"
-                                >
+                                <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
                                     {{ item.price }}
                                 </td>
-                                <td
-                                    class="whitespace-nowrap px-3 py-3 text-center font-bold"
-                                >
+                                <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
                                     {{ item.weight }}
                                 </td>
-                                <td
-                                    class="whitespace-nowrap px-3 py-3 text-center font-bold"
-                                >
+                                <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
                                     {{ item.mobis }}
                                 </td>
-                                <td
-                                    class="whitespace-nowrap px-3 py-3 text-center font-bold"
-                                >
+                                <td class="whitespace-nowrap px-3 py-3 text-center font-bold">
                                     {{ item.korea }}
                                 </td>
                                 <td class="whitespace-nowrap w-24">
-                                    <div
-                                        class="flex justify-center gap-1 items-center px-2"
-                                    >
+                                    <div class="flex justify-center gap-1 items-center px-2">
                                         <a :href="route('goods.edit', item.id)">
-                                            <i
-                                                class="material-icons text-blue-500"
-                                                >create</i
-                                            >
+                                            <i class="material-icons text-blue-500 hover:text-blue-700">create</i>
                                         </a>
                                         <form @submit.prevent="remove(item.id)">
                                             <button type="submit">
-                                                <i
-                                                    class="material-icons text-red-600"
-                                                    >delete_forever</i
-                                                >
+                                                <i class="material-icons text-red-600 hover:text-red-800">delete_forever</i>
                                             </button>
                                         </form>
                                     </div>
                                 </td>
                             </tr>
-                            <tr
-                                v-else
-                                class="transition duration-300 ease-in-out bg-neutral-200"
-                            >
-                                <td
-                                    colspan="14"
-                                    class="whitespace-nowrap px-3 py-3 text-center text-red-500 font-bold"
-                                >
-                                    <i class="material-icons text-red-500"
-                                        >mood_bad</i
-                                    >
+                            <tr v-else class="transition duration-300 ease-in-out bg-neutral-200">
+                                <td colspan="14" class="whitespace-nowrap px-3 py-3 text-center text-red-500 font-bold">
+                                    <i class="material-icons text-red-500">mood_bad</i>
                                     <br />
                                     !متاسفانه چیزی برای نمایش در پایگاه داده
                                     موجود نیست
@@ -301,24 +241,15 @@ const print = (data) => {
                             </tr>
                         </tbody>
                     </table>
-                    <div
-                        v-if="goods.length > 0"
-                        class="flex justify-center py-3"
-                    >
+                    <div v-if="goods.length > 0" class="flex justify-center py-3">
                         <ul class="flex">
-                            <li
-                                class="bg-blue-400 hover:bg-blue-300 mx-2 flex justify-center items-center px-3 py-2 rounded-md cursor-pointer"
-                                id="prev"
-                                @click="page('prev')"
-                            >
-                                <i class="material-icons">fast_rewind</i>
+                            <li class="bg-blue-400 hover:bg-blue-300 mx-2 flex justify-center items-center px-3 py-2 rounded-md cursor-pointer"
+                                id="prev" @click="page('prev')">
+                                <i class="material-icons text-white">fast_rewind</i>
                             </li>
-                            <li
-                                class="bg-blue-400 hover:bg-blue-300 mx-2 flex justify-center items-center px-3 p-2 rounded-md cursor-pointer"
-                                id="next"
-                                @click="page('next')"
-                            >
-                                <i class="material-icons">fast_forward</i>
+                            <li class="bg-blue-400 hover:bg-blue-300 mx-2 flex justify-center items-center px-3 p-2 rounded-md cursor-pointer"
+                                id="next" @click="page('next')">
+                                <i class="material-icons text-white">fast_forward</i>
                             </li>
                         </ul>
                     </div>

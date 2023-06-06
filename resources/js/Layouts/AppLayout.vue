548434120b20d0f2ba5a96c39979c2e1c7d8f53d
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-06 11:17:42 +0330

    Add icons for the navigation menu links

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -34,21 +34,27 @@ const logout = () => {
                     class="material-icons absolute m-3 left-0 hover:cursor-pointer">close</i>
                 <ul class="rtl flex flex-col pt-5">
                     <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                        <i class="px-2 material-icons hover:cursor-pointer">search</i>
                         جستجوی اجناس
                     </NavLink>
                     <NavLink :href="route('goods')" :active="route().current('goods')">
+                        <i class="px-2 material-icons hover:cursor-pointer">local_mall</i>
                         اجناس
                     </NavLink>
                     <NavLink :href="route('rates')" :active="route().current('rates')">
+                        <i class="px-2 material-icons hover:cursor-pointer">show_chart</i>
                         نرخ های ارز
                     </NavLink>
                     <NavLink :href="route('relations')" :active="route().current('relations')">
+                        <i class="px-2 material-icons hover:cursor-pointer">sync</i>
                         تعریف رابطه اجناس
                     </NavLink>
                     <NavLink :href="route('price.give')" :active="route().current('price.give')">
+                        <i class="px-2 material-icons hover:cursor-pointer">receipt</i>
                         قیمت گذاری
                     </NavLink>
                     <NavLink :href="route('profile.show')" :active="route().current('profile')">
+                        <i class="px-2 material-icons hover:cursor-pointer">portrait</i>
                         جساب کاربری
                     </NavLink>
                 </ul>
@@ -56,6 +62,7 @@ const logout = () => {
                 <form @submit.prevent="logout">
                     <button type="submit"
                         class="rtl inline-flex items-end py-3 pr-6 text-sm font-medium leading-5 text-gray-500 hover:bg-indigo-500 w-full hover:text-white focus:outline-none transition duration-150 ease-in-out">
+                        <i class="px-2 material-icons hover:cursor-pointer">power_settings_new</i>
                         خروج از حساب
                     </button>
                 </form>

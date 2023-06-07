519dd8b32af0ec609c65fb8073fafb6a579d2ac0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-07 12:43:54 +0330

    Update the controller function

--- resources/js/Pages/Price/Partials/Load.vue
+++ resources/js/Pages/Price/Partials/Load.vue
@@ -58,6 +58,6 @@ input:checked+label:after {
 
 input[name='panel']:checked~.accordion__content {
   /* Get this as close to what height you expect */
-  max-height: 50em;
+  max-height: 150em;
 }
 </style>

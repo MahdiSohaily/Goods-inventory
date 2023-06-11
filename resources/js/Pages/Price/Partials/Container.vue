911f48ef1f45431d1e7ca8698970fd673ecf7add
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-11 09:36:19 +0330

    Add script to display tooltip for stock

---
+++ resources/js/Pages/Price/Partials/Container.vue
@@ -0,0 +1,25 @@
+<script setup>
+import CodeInfo from "./CodeInfo.vue";
+import RelationsDetails from "./RelationsDetails.vue";
+import GivenPrice from "./GivenPrice.vue";
+import Estelam from "./Estelam.vue";
+
+const props = defineProps({
+    information: Array,
+    partNumber: String,
+    relation: Array,
+    exist: String,
+  completeCode: String,
+  rates: Array,
+});
+
+</script>
+<template>
+    <CodeInfo :information="existing[code][index]['information']" :partNumber="index" />
+    <RelationsDetails :rates="props.rates" :relation="existing[code][index]['relation']"
+        :exist="existing[code][index]['exist']" @setPrice="(value) => updatePrice(value)" />
+    <GivenPrice :information="existing[code][index]['information']" :givenPrice="existing[code][index]['givenPrice']"
+        :price="price" />
+    <!--  <Estelam /> -->
+</template>
+<style></style>
--- resources/js/Pages/Price/Partials/Load.vue
+++ resources/js/Pages/Price/Partials/Load.vue
@@ -1,10 +1,7 @@
 <script setup>
 import { onMounted, onUpdated, ref } from 'vue';
 import AppLayout from "@/Layouts/AppLayout.vue";
-import CodeInfo from "./CodeInfo.vue";
-import RelationsDetails from "./RelationsDetails.vue";
-import GivenPrice from "./GivenPrice.vue";
-import Estelam from "./Estelam.vue";
+import Container from './Container.vue';
 
 const props = defineProps({
   explodedCodes: Array,
@@ -15,15 +12,6 @@ const props = defineProps({
   rates: Array,
 });
 
-const price = ref(0);
-
-const updatePrice = (value) => {
-  price.value = value;
-}
-
-onUpdated(() => {
-  console.log(price.value)
-})
 </script>
 
 <template>
@@ -33,17 +21,15 @@ onUpdated(() => {
       <!--  Panel 1  -->
       <div v-for="code, index of explodedCodes">
         <input type="checkbox" checked="true" name="panel" :id="index" class="hidden">
-        <label :for="index" class="relative flex items-center bg-gray-700 text-white p-4 shadow border-b border-grey hover:cursor-pointer">
+        <label :for="index"
+          class="relative flex items-center bg-gray-700 text-white p-4 shadow border-b border-grey hover:cursor-pointer">
           {{ code }}
         </label>
         <div class="accordion__content overflow-hidden bg-grey-lighter">
           <div v-for="item, index of existing[code]" class="grid grid-cols-1 md:grid-cols-10 gap-6 lg:gap-2 lg:p-2">
-            <CodeInfo :information="existing[code][index]['information']" :partNumber="index" />
-            <RelationsDetails :rates="props.rates" :relation="existing[code][index]['relation']"
-              :exist="existing[code][index]['exist']" @setPrice="(value) => updatePrice(value)" />
-            <GivenPrice :information="existing[code][index]['information']"
-              :givenPrice="existing[code][index]['givenPrice']" :price="price" />
-            <!--  <Estelam /> -->
+            <Container :information="existing[code][index]['information']" :partNumber="index" :rates="props.rates"
+              :relation="existing[code][index]['relation']" :exist="existing[code][index]['exist']"
+              :givenPrice="existing[code][index]['givenPrice']" />
           </div>
         </div>
       </div>

90e30a3b297a00846bcedd0426c9f35eb0a2e654
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-11 12:58:40 +0330

    Add script to update the price value for every specific card

--- resources/js/Pages/Price/Partials/GivenPrice.vue
+++ resources/js/Pages/Price/Partials/GivenPrice.vue
@@ -35,7 +35,7 @@ const savePrice = () => {
         errorBag: "savePrice",
         preserveScroll: true,
         onSuccess: () => {
-            console.log('Wll done');
+            form.price = null;
         },
     });
 };
@@ -137,10 +137,7 @@ onUpdated(() => {
                     <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
                         ثبت
                     </PrimaryButton>
-                    <a
-                        class="hover:cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
-                        ارسال به نیایش
-                    </a>
+
                     <PrimaryButton @click="$emit('setPrice', 'نداریم')" :class="{ 'opacity-25': form.processing }"
                         :disabled="form.processing">
                         نداریم !!!
@@ -148,6 +145,10 @@ onUpdated(() => {
                     <ActionMessage :on="form.recentlySuccessful" class="mr-3">
                         عملیات موفقانه صورت گرفت.
                     </ActionMessage>
+                    <a
+                        class="hover:cursor-pointer inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                        ارسال به نیایش
+                    </a>
                 </template>
             </FormRelation>
         </div>

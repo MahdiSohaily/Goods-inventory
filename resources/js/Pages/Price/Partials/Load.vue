511b3dad92b86b5ba1ccaef0f657c10abd393aea
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-06-13 13:47:37 +0330

    Add script to show error message for the non existing codes

--- resources/js/Pages/Price/Partials/Load.vue
+++ resources/js/Pages/Price/Partials/Load.vue
@@ -19,29 +19,40 @@ const props = defineProps({
     <div class="accordion mt-12">
       <!--  Panel 1  -->
       <div v-for="code, code_index of explodedCodes">
-        <input type="checkbox" checked="true" name="panel" :id="code_index" class="hidden">
-        <label :for="code_index"
-          class="relative flex items-center bg-gray-700 text-white p-4 shadow border-b border-grey hover:cursor-pointer">
-          {{ code }}
-          {{ existing[code] }}
-        </label>
-        <div class="accordion__content overflow-hidden bg-grey-lighter">
-          <div v-for="item, index of existing[code]" class="grid grid-cols-1 md:grid-cols-10 gap-6 lg:gap-2 lg:p-2">
-            <Container :code="code" :index="index" :existing="existing"
-              :information="existing[code][index]['information']" :partNumber="index" :rates="props.rates"
-              :relation="existing[code][index]['relation']" :exist="existing[code][index]['exist']"
-              :notification="notification" :givenPrice="existing[code][index]['givenPrice']" :customer="customer"
-              :completeCode="completeCode" />
-          </div>
-        </div>
-        <div v-if="not_exist.indexOf(code) > -1" class="accordion__content overflow-hidden bg-grey-lighter">
 
-          <div class="bg-white rounded-lg px-3 py-10">
-            <p class="text-center">
-              کد وارد شده در سیستم موجود نیست
-            </p>
+        <template v-if="existing[code] && Object.keys(existing[code]).length">
+          <input type="checkbox" checked="true" name="panel" :id="code_index" class="hidden">
+          <label :for="code_index"
+            class="relative flex items-center bg-gray-700 text-white p-4 shadow border-b border-grey hover:cursor-pointer">
+            {{ code }}
+          </label>
+          <div class="accordion__content overflow-hidden bg-grey-lighter">
+            <div v-for="item, index of existing[code]" class="grid grid-cols-1 md:grid-cols-10 gap-6 lg:gap-2 lg:p-2">
+              <Container :code="code" :index="index" :existing="existing"
+                :information="existing[code][index]['information']" :partNumber="index" :rates="props.rates"
+                :relation="existing[code][index]['relation']" :exist="existing[code][index]['exist']"
+                :notification="notification" :givenPrice="existing[code][index]['givenPrice']" :customer="customer"
+                :completeCode="completeCode" />
+            </div>
           </div>
-        </div>
+        </template>
+        <template v-if="not_exist.indexOf(code) > -1">
+          <input type="checkbox" checked="true" name="panel" :id="code_index" class="hidden">
+          <label :for="code_index"
+            class="relative flex items-center bg-gray-700 text-white p-4 shadow border-b border-grey hover:cursor-pointer">
+            {{ code }}
+          </label>
+          <div class="accordion__content overflow-hidden bg-grey-lighter">
+
+            <div class="bg-white rounded-lg px-3 py-10">
+              <p class="text-center">
+                کد وارد شده در سیستم موجود نیست
+              </p>
+            </div>
+          </div>
+        </template>
+
+
       </div>
     </div>
   </AppLayout>
